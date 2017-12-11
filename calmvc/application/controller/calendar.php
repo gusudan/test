fdc1e82e9fbaf5ff5aff41b43d21ae28460399b9
Dan Gusu <dan.gusu@sinoptix.com>
2017-12-11 17:03:05 +0200

    luni

--- calmvc/application/controller/calendar.php
+++ calmvc/application/controller/calendar.php
@@ -3,11 +3,20 @@
 class Calendar extends Controller {
       
     public function index() {
-        $alldata = date("mY");
-        $an = date("Y");
-        $luna = date("n");
-        $totalzile = date("t");
-        $primazi = date('w',strtotime(date('01-m-Y')));
+        $date = new DateTime();   
+        
+        $zi = $date->format("d");
+        $an = $date->format("Y");
+        $luna = $date->format("m");
+        $lastday = $date->format("t");
+        
+        $firstday = $date->modify('first day of')->format("w");
+        
+             
+        $nextmonth = $date->add(new DateInterval('P1M'))->format("Ymd");
+        $prevmonth = $date->sub(new DateInterval('P2M'))->format("Ymd");
+        
+        
         
         if ($_SESSION['isloggedin'] == FALSE) {
             $_SESSION['msg'] = "Trebuie sa va logati.";
@@ -20,20 +29,30 @@ class Calendar extends Controller {
         require APP . 'views/_templates/footer.php';
     }
     
-    public function data($data){
-        if (!isset($data)){
-            $this->an = date("Y");
-            $this->luna = date("n");
-            $this->totalzile = date("t");
-            $this->primazi = date('w',strtotime(date('01-m-Y')));
+    public function data($dat){
+        $newdat = date('Ymd', strtotime($dat));
+        $date = new DateTime($newdat);   
+         
+        $zi = $date->format("d");
+        $an = $date->format("Y");
+        $luna = $date->format("m");
+        $lastday = $date->format("t");
+        
+        $firstday = $date->modify('first day of')->format("w");
+        
+             
+        $nextmonth = $date->add(new DateInterval('P1M'))->format("Ymd");        
+        $prevmonth = $date->sub(new DateInterval('P2M'))->format("Ymd");
+        
+        if ($_SESSION['isloggedin'] == FALSE) {
+            $_SESSION['msg'] = "Trebuie sa va logati.";
+            require APP . 'views/_templates/header.php';
+            require APP . 'views/problem/index.php';
         } else {
-            $data = strtotime($data);
-            $an = date("Y",$data);
-            $luna = date("n",$data);
-            
             require APP . 'views/_templates/header.php';
             require APP . 'views/calendar/index.php';
         }
+        require APP . 'views/_templates/footer.php';
         
     }    
     
